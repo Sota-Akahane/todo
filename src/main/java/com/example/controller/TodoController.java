6a108782779ed77aad50bc96ed516aac6750b0d0
Sota-Akahane <sota258@docomo.ne.jp>
2025-06-17 22:22:48 +0900

    タスク一覧を表示する機能を実装しました。

--- src/main/java/com/example/controller/TodoController.java
+++ src/main/java/com/example/controller/TodoController.java
@@ -1,11 +1,15 @@
 package com.example.controller;
 
+import com.example.domain.Todo;
 import com.example.service.TodoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 /**
  * タスク関連の処理の制御を行うコントローラクラスです.
@@ -13,20 +17,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * @author sota-akahane
  */
 @Controller
-@RequestMapping("")
+@RequestMapping("/")
 public class TodoController {
     @Autowired
     private TodoService todoService;
 
     @GetMapping("/showTodoList")
-    public String showTodoList() {
+    public String showTodoList(Model model) {
+        List<Todo> todoList = todoService.findAll();
+        model.addAttribute("todoList", todoList);
         return "todoList";
     }
 
     @PostMapping("/addTodo")
-    public String addTodo(String title, String description) {
+    public String addTodo(String title, String description, Model model) {
         todoService.addTodo(title, description);
 
-        return "todoList";
+        return showTodoList(model);
     }
 }
--- src/main/java/com/example/service/TodoService.java
+++ src/main/java/com/example/service/TodoService.java
@@ -1,16 +1,32 @@
 package com.example.service;
 
+import com.example.domain.Todo;
 import com.example.repository.TodoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 /**
  * タスク関連の業務処理を行うサービスクラスです.
  *
  * @author sota-akahane
  */
+@Service
+@Transactional
 public class TodoService {
     @Autowired
     private TodoRepository todoRepository;
+
+    /**
+     * タスク一覧を取得します.
+     *
+     * @return タスク一覧
+     */
+    public List<Todo> findAll() {
+        return todoRepository.findAll();
+    }
 
     /**
      * タスクを登録します.
